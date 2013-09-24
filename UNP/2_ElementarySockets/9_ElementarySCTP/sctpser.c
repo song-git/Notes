d38d4c7d13c5626b2e00cf638c69fc6d4c6c76c7
song <song@localhost.localdomain>
2013-09-24 20:17:38 +0800

    Initial commit

---
+++ UNP/2_ElementarySockets/9_ElementarySCTP/sctpser.c
@@ -0,0 +1,49 @@
+#include<netinet/in.h>
+#include<netinet/sctp.h>
+#include<sys/socket.h>
+
+#define SA struct sockaddr
+#define LISTENQ 5
+#define BUFFSIZE 1024
+
+int main(int argc, char **argv)
+{
+	int sock_fd, msg_flags;
+	char readbuf[BUFFSIZE];
+	struct sockaddr_in servaddr, cliaddr;
+	struct sctp_sndrcvinfo sri;
+	struct sctp_event_subscribe evnts;
+	int stream_increment = -1;
+	socklen_t len;
+	size_t rd_sz;
+
+	if(argc == 2)
+		stream_increment = atoi(argv[1]);
+	sock_fd = socket(AF_INET, SOCK_SEQPACKET, IPPROTO_SCTP);
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port = htons();
+
+	bind(sock_fd, (SA *)&servaddr, sizeof(servaddr));
+	bzero(&evnts, sizeof(evnts));
+	evnts.sctp_data_io_event = 1;
+	setsockopt(sock_fd, IPPROTO_SCTP, SCTP_EVENTS, &evnts, sizeof(evnts));
+
+	listen(sock_fd, LISTENQ);
+	for(;;) {
+		len = sizeof(struct sockaddr_in);
+		rd_sz = sctp_recvmsg(sock_fd, readbuf, sizeof(readbuf), 
+				(SA *)&cliaddr, &len, &sri, &msg_flags);
+		if(stream_increment) {
+			sri.sinfo_stream ++;
+			if(sri.sinfo_stream >= 
+					sctp_get_no_strms(sock_fd, (SA *)&cliaddr, len))
+				sri.sinfo_stream = 0;
+		}
+		sctp_sendmsg(sock_fd, readbuf, rd_sz,
+				(SA *)&cliaddr, len, 
+				sri.sinfo_ppid, sri.sinfo_flags, 
+				sri.sinfo_stream, 0, 0);
+	}
+}
